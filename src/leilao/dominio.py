f4a0c475d64d41c19c026c848a9b0213086c0b39
Pollyana <pollyanarocha416@gmail.com>
2023-03-30 11:56:32 -0400

    testes

---
+++ src/leilao/dominio.py
@@ -0,0 +1,45 @@
+import sys
+
+class Usuario:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+
+class Lance:
+
+    def __init__(self, usuario, valor):
+        self.usuario = usuario
+        self.valor = valor
+
+
+class Leilao:
+
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.__lances = []
+    
+    def propoe(self, lance: Lance):
+        self.__lances.append(lance)
+        
+    @property
+    def lances(self) -> list:
+        return self.__lances[:]#copia da lista
+
+class Avaliador:
+    
+    def __init__(self) -> None:
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
+
+    def avalia(self, leilao: Leilao):
+        for lance in leilao.lances:
+            if lance.valor > self.maior_lance:
+                self.maior_lance = lance.valor
+            elif lance.valor < self.menor_lance:
+                self.menor_lance = lance.valor
+            
---
+++ src/leilao/principal.py
@@ -0,0 +1,19 @@
+from dominio import Usuario, Lance, Leilao, Avaliador
+
+gui = Usuario('Gui')
+yuri = Usuario('Yuri')
+
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
+
+leilao = Leilao('Celular')
+
+leilao.lances.append(lance_do_gui)
+leilao.lances.append(lance_do_yuri)
+
+for lance in leilao.lances:
+    print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+print(f'o menor lance: {avaliador.menor_lance} e o maior: {avaliador.maior_lance}')
