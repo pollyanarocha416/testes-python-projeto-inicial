998cb5afb864c13b9542f3d558274028ec35cde7
Pollyana <pollyanarocha416@gmail.com>
2023-03-31 11:40:48 -0400

    concluido

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -17,29 +17,21 @@ class Lance:
         self.valor = valor
 
 
-class Leilao:
-
-    def __init__(self, descricao):
-        self.descricao = descricao
-        self.__lances = []
-    
-    def propoe(self, lance: Lance):
-        self.__lances.append(lance)
         
-    @property
-    def lances(self) -> list:
-        return self.__lances[:]#copia da lista
-
-class Avaliador:
+class Leilao: 
+    def __init__(self, descricao): 
+        self.descricao = descricao 
+        self.__lances = [] 
+        self.maior_lance = sys.float_info.min 
+        self.menor_lance = sys.float_info.max 
     
-    def __init__(self) -> None:
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
-
-    def avalia(self, leilao: Leilao):
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            elif lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
-            
+    def propoe(self, lance: Lance): 
+        if lance.valor > self.maior_lance: 
+            self.maior_lance = lance.valor 
+        if lance.valor < self.menor_lance: 
+            self.menor_lance = lance.valor 
+            self.__lances.append(lance) 
+    
+    @property 
+    def lances(self): 
+        return self.__lances[:]
--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -1,4 +1,4 @@
-from dominio import Usuario, Lance, Leilao, Avaliador
+from dominio import Usuario, Lance, Leilao
 
 gui = Usuario('Gui')
 yuri = Usuario('Yuri')
@@ -14,6 +14,4 @@ leilao.lances.append(lance_do_yuri)
 for lance in leilao.lances:
     print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
 
-avaliador = Avaliador()
-avaliador.avalia(leilao)
-print(f'o menor lance: {avaliador.menor_lance} e o maior: {avaliador.maior_lance}')
+
